9f4eb6908360c6efb2496ea42e16af51118bb44d
Milan Kathiriya <milankathiriya@Milans-MacBook-Pro.local>
2020-04-29 10:10:06 +0530

    updated

--- student_follow_up/lib/pages/LeadsPage.dart
+++ student_follow_up/lib/pages/LeadsPage.dart
@@ -7,7 +7,6 @@ import '../globals/StudentDetails.dart';
 import '../globals/StaffCredentials.dart';
 
 class LeadsPage extends StatefulWidget {
-
   @override
   _LeadsPageState createState() => _LeadsPageState();
 }
@@ -20,31 +19,25 @@ class _LeadsPageState extends State<LeadsPage> {
   @override
   Widget build(BuildContext context) {
 
-    if (grid.number == null) {
-      setState(() {
-        _futureStudent = null;
-      });
-    } else {
-      setState(() {
-        _futureStudent = getLeads(staffCredentials.userName);
-      });
-    }
+    setState(() {
+      _futureStudent = getLeads(staffCredentials.userName);
+    });
 
     return Scaffold(
       body: Column(
         children: <Widget>[
           (_futureStudent == null)
               ? Expanded(
-            child: Center(
-              child: Text(
-                "Please select GRID first",
-                style: TextStyle(fontSize: 20, color: Colors.red),
-              ),
-            ),
-          )
+                  child: Center(
+                    child: Text(
+                      "Please select GRID first",
+                      style: TextStyle(fontSize: 20, color: Colors.red),
+                    ),
+                  ),
+                )
               : Expanded(
-            child: mdp.getLeadsData(grid.number, _futureStudent),
-          ),
+                  child: mdp.getLeadsData(grid.number, _futureStudent),
+                ),
         ],
       ),
     );
